f0191c24e29be470415024f74e9fc6fbce557413
Mohamed Alaa <m.3laa.95@gmail.com>
2021-12-11 12:33:12 +0200

    fix

--- bin/push-server.php
+++ bin/push-server.php
@@ -16,7 +16,7 @@ $pull->bind('tcp://127.0.0.1:5555'); // Binding to 127.0.0.1 means the only clie
 $pull->on('message', array($pusher, 'onBlogEntry'));
 
 // Set up our WebSocket server for clients wanting real-time updates
-$webSock = new React\Socket\Server('org1.tamkeentechlab.com:8090', $loop); // Binding to 0.0.0.0 means remotes can connect
+$webSock = new React\Socket\Server('34.72.63.157:8090', $loop); // Binding to 0.0.0.0 means remotes can connect
 $webServer = new Ratchet\Server\IoServer(
     new Ratchet\Http\HttpServer(
         new Ratchet\WebSocket\WsServer(
